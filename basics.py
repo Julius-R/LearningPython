893a134e9f23d007b99b659179b4fccdc8f5063d
Julius-R <julius.robinson95@yahoo.com>
2021-04-11 20:01:01 -0700

    edited filename to variables

--- basics.py
+++
@@ -1,7 +0,0 @@
-# Variables
-message = "Welcome, humans"
-player_name = "tom riddle"
-userName = "JR403W3-G005"
-print(player_name.title())
-
-
---
+++ variables.py
@@ -0,0 +1,10 @@
+# Variables
+message = "Welcome, humans"
+player_name = "tom riddle"
+userName = "  JR403W3-G005   " #Adding whitespace
+age = 35
+
+print(player_name.title())
+print(userName.strip()) # Stripping whitespace
+print(player_name.title() + " is " + str(age)) #str converts int to string
+
