3f8b0698ab517720a0f0179bc281a355b738196e
Julius-R <julius.robinson95@yahoo.com>
2021-04-13 19:15:37 -0700

    studied lists

--- lists.py
+++ lists.py
@@ -25,3 +25,30 @@ print(names)
 names.remove('eren')
 print(names)
 # Special note, remove only removes the FIRST instance in the list
+
+#  List organization
+names = ['jason', 'hagrid', 'marik'] # reinitalization for sorting
+print("Unsorted list of names: " + str(names))
+# sorted method, allows for sorting temporarily
+print(sorted(names))
+
+# sort method, permanately sorts the list
+names.sort()
+names.sort(reverse = True) # allows sort in reverse alphabetical order
+print(names)
+
+# reverse method, allows you to reverse a list's order
+print(names.reverse()) # note, it reverses the ORDER of the list, not alphabetically
+
+print(len(names)) # prints the length of the list
+
+# looping through a list
+userInput = ['jotaro', 'amelia', 'ryo']
+for name in userInput:
+    names.append(name)
+    print(name.title() + ", welcome to the team!")
+
+print(names)
+
+# dealing with lists of numbers in python
+# digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
