8acae5f25e600a480a3f00e59b1f83e54f622093
Julius-R <julius.robinson95@yahoo.com>
2021-04-18 22:05:06 -0700

    file structure change

--- variables.py
+++ variables.py
@@ -8,3 +8,12 @@ print(player_name.title())
 print(userName.strip()) # Stripping whitespace
 print(player_name.title() + " is " + str(age)) #str converts int to string
 
+# Casting
+x = int(43)
+price = round(float((age * 345) / x), 2)
+
+def displayTotal():
+    total = "Your total is: " + str(price)
+    return total
+
+print(displayTotal())
