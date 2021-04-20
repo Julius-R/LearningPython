a932567064b54ee50993026b2a7b490d2d69f158
Julius-R <julius.robinson95@yahoo.com>
2021-04-19 22:32:56 -0700

    some more practice on collection concepts

--- practice/collections.py
+++ practice/collections.py
@@ -14,3 +14,10 @@ numsMultipliedByFour = [multiplyNums(i, 4) for i in numbers ]
 displayItems(numbers)
 print(bufferer)
 displayItems(numsMultipliedByFour)
+
+myTuple = ('apple', 'cherry', 'orange', 'peach', 'grape', 'mango')
+searchValue = input('What fruit are you looking for? : ')
+if searchValue in myTuple:
+    print(searchValue + " exists within the tuple")
+else:
+    print(searchValue + " does not exist")
