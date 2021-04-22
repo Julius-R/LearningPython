d0d1a1c03452b86e071be6ce6a206fac5da5e8b2
Julius-R <julius.robinson95@yahoo.com>
2021-04-21 22:01:03 -0700

    worked on practicing lists

--- practice/collections.py
+++ practice/collections.py
@@ -1,23 +1,41 @@
 bufferer = "--------------------------------"
-def displayItems(items):
+def display_items(items):
     for x in items:
         print(x)
         
-def multiplyNums(num, amount):
+def multiply_nums(num, amount):
     return num * amount
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-numsMultipliedByFour = [multiplyNums(i, 4) for i in numbers ]
+nums_multiplied_by_four = [multiply_nums(i, 4) for i in numbers ]
 
 
-displayItems(numbers)
+display_items(numbers)
 print(bufferer)
-displayItems(numsMultipliedByFour)
+display_items(nums_multiplied_by_four)
 
 myTuple = ('apple', 'cherry', 'orange', 'peach', 'grape', 'mango')
-searchValue = input('What fruit are you looking for? : ')
-if searchValue in myTuple:
-    print(searchValue + " exists within the tuple")
-else:
-    print(searchValue + " does not exist")
+search_values = list()
+
+def get_input_from_user():
+    num_vals_to_add = input('How many values to add?')
+    count = 0
+    while count < int(num_vals_to_add):
+        value = input('What would you like to add to the array? : ')
+        search_values.append(value)
+        count += 1
+    
+def does_val_exist(arr):
+    val_exists = False
+    for val in arr:
+        if val in myTuple:
+            val_exists = True
+            break
+        else:
+            val_exists = False
+    return val_exists
+
+get_input_from_user()
+display_items(search_values)
+print(does_val_exist(search_values))
