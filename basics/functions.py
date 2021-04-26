9a73e2626e3d4a2a7abb121d802c1af6f890508d
Julius-R <julius.robinson95@yahoo.com>
2021-04-25 18:00:39 -0700

    more study time

---
+++ basics/functions.py
@@ -0,0 +1,36 @@
+# functions work the same as in most other languages, here are some differences I found
+# Arbitrary Arguments - *args
+def say_names(*names):
+    print(names[1])
+    
+say_names('Jake', 'Miles')
+
+# keyword arguments provide a way to have arguments provided out of order
+def keyword_args(name1, name3, name2,):
+    print(name3)
+    
+keyword_args(name2 = 'Jake', name1 = 'Janice', name3 = 'Paul')
+
+# Arbitrary keyword args **kwargs
+def arbitrary(**args):
+    print(args['name'])
+    
+arbitrary(name = 'Jane', lname = 'Pike')
+
+# Pass - used in a function since functions can not be empty
+def rando_func():
+    pass
+
+# lambdas
+# A lambda is an anonymous function that can take numerous args and only one expression
+# Syntax - lambda arguments : expression
+x = lambda a : a + 10
+print(x(4))
+
+def my_lambda(n):
+    return lambda a : a * n
+
+my_tripler = my_lambda(3)
+my_doubler = my_lambda(2)
+print(my_doubler(5))
+print(my_tripler(5))
--- collections/sets.py
+++ collections/sets.py
@@ -1,1 +1,31 @@
-# Sets are used to store mult
+# Sets are used to store multiple values in a single variable, similar to a tuple
+# The difference is that sets are unordered and unindexed and can not have duplicate values
+demo_set = {'apple', 'mango','cherry'}
+
+# You can not access values in a set aside from looping or using the in keyword to check if a value is present
+print("apple" in demo_set)
+for val in demo_set:
+    print(val)
+    
+# Adding a value to the set
+demo_set.add('bananas')
+print(demo_set)
+
+# Adding another set
+second_set = {'kiwi', 'peach'}
+demo_set.update(second_set)
+print(demo_set)
+
+# note: update method can be used on any iterable
+
+# You can delete an item from a set using either remove or discard
+# remove will throw an error if item does not exist whereas discard will not
+demo_set.remove('apple')
+demo_set.discard('kiwi')
+
+# clear will empty a set and del will delete the set entirely
+demo_set.clear()
+print(demo_set)
+del demo_set
+# print('set is: ' + demo_set) Will throw error since set does not exist
+
